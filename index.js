ea0867a3c61f37befe5cc916720ff782dbeebd81
Apoorv Kansal <apoorvkansalak@gmail.com>
2020-12-25 01:59:05 +0530

    Initial setup of files

---
+++ index.js
@@ -0,0 +1,12 @@
+const quill = new Quill('#quillEditor', {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+      ]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow'  // or 'bubble'
+  });
+  
