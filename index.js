07a9896d6ef454d5b136e24f97f93a980827d990
Apoorv Kansal <apoorvkansalak@gmail.com>
2020-12-28 12:31:09 +0530

    [feat] implemented a basic menubar that follows user

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const text = document.getElementById('text')
+let showTip = false
+
 const quill = new Quill('#quillEditor', {
     modules: {
       toolbar: [
@@ -6,7 +9,40 @@ const quill = new Quill('#quillEditor', {
         ['image', 'code-block']
       ]
     },
-    placeholder: 'Compose an epic...',
+    placeholder: 'Type / for commands...',
     theme: 'snow'  // or 'bubble'
   });
-  
+
+
+/**
+ * Main task is to show a small menubar and then depending on the selection format the quill settings
+ */
+quill.on('text-change', function(delta, oldDelta, source) {
+  if (source == 'user') {
+		const {ops} = delta
+		// Two cases can be there, if only starting, then ops would contain only one array
+		if(ops[0] && ops[0].insert && ops[0].insert === '/'){
+			console.log('Setting to true')
+			const bounds = quill.getBounds(0)
+			
+			text.style.top = `${bounds.top+105}px`
+			text.style.left = `${bounds.left+20}px`
+			// Show the menu bar
+			// Ask the user what he wants
+			text.style.display = "flex"
+		}
+		else if(ops[1] && ops[1].insert && ops[1].insert === '/'){
+			console.log('Setting to true, 2nd case')
+			console.log(quill.getBounds(ops[0].retain))
+			const bounds = quill.getBounds(ops[0].retain)
+			text.style.top = `${bounds.top+105}px`
+			text.style.left = `${bounds.left+30}px`
+			text.style.display = "flex"
+		}
+		else{
+			text.style.top = 0
+			text.style.left = 0
+			text.style.display = "none"
+		}
+	}
+});
